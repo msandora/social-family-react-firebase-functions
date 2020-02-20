88587a1a964a9c7a54f4fd5c6a3a6cdeecb93c66
msandora <mark.sandora@gmail.com>
2020-02-20 00:44:50 -0500

    add thumbnails

--- functions/handlers/recipes.js
+++ functions/handlers/recipes.js
@@ -1,4 +1,5 @@
 const { admin, db } = require('../util/admin');
+const config = require('../util/config');
 
 const {
   validateRecipeData
@@ -147,7 +148,6 @@ exports.commentOnRecipe = (req, res) => {
     });
 };
 
-
 /*********************** 
 // Like a recipe
 Get: /api/recipe/MVz7Dhjkc3jjLHCFhpAV/like
@@ -276,4 +276,69 @@ exports.deleteRecipe = (req, res) => {
       console.error(err);
       return res.status(500).json({ error: err.code });
     });
+};
+
+
+
+// Upload a profile image for user
+exports.uploadRecipeImage = (req, res) => {
+  const BusBoy = require('busboy');
+  const path = require('path');
+  const os = require('os');
+  const fs = require('fs');
+
+  const busboy = new BusBoy({ headers: req.headers });
+
+  let imageToBeUploaded = {};
+  let imageFileName;
+
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    console.log(fieldname, file, filename, encoding, mimetype);
+    if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
+      return res.status(400).json({ error: 'Wrong file type submitted' });
+    }
+    // my.image.png => ['my', 'image', 'png']
+    const imageExtension = filename.split('.')[filename.split('.').length - 1]; // gives use the value of png/jpg
+    // 32756238461724837.png
+    imageFileName = `${Math.round(
+      Math.random() * 1000000000000
+    ).toString()}.${imageExtension}`;
+    
+    const filepath = path.join(os.tmpdir(), imageFileName);
+
+    imageToBeUploaded = { filepath, mimetype };
+    console.log("filepath", filepath);
+
+    file.pipe(fs.createWriteStream(filepath));
+  });
+  busboy.on('finish', () => {
+    admin
+      .storage()
+      .bucket(config.storageBucket)
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype
+          }
+        }
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${
+          config.storageBucket
+        }/o/recipe-images%2F${imageFileName}?alt=media`;
+        console.log("imageUrl", imageUrl);
+        console.log("imageFileName", imageFileName);
+//gs://socialfamily-d58c8.appspot.com/recipe-images
+        // return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: 'image uploaded successfully' });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: 'something went wrong' });
+      });
+  });
+  busboy.end(req.rawBody);
 };
