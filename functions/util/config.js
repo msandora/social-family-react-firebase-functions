09901ca7ba2919df5ed101a313d779cca6242180
Ahmed Hadjou <ahmed@leapfrog.ltd>
2020-06-14 11:56:57 +0100

    Removed env files from git

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
- // Your web app's Firebase configuration
-module.exports = {
-  apiKey: "AIzaSyAyCj6L0iNt7kJnxVtqrq3Y5MZ39CFiy9A",
-  authDomain: "socialfamily-d58c8.firebaseapp.com",
-  databaseURL: "https://socialfamily-d58c8.firebaseio.com",
-  projectId: "socialfamily-d58c8",
-  storageBucket: "socialfamily-d58c8.appspot.com",
-  messagingSenderId: "581383764882"
-};
